c36cea3224718b4aee3e7347afe78263e75830d0
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2021-10-20 09:36:04 +0700

    boilerplate

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+};
